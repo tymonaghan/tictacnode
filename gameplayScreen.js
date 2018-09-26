0efb98eb3771334f377bd9fdf5d9be7328851bc8
Tyler Monaghan <ty.monaghan@gmail.com>
2018-09-26 14:33:53 -0500

    made buttons for each square of the board. trying to space them out.

--- gameplayScreen.js
+++ gameplayScreen.js
@@ -11,15 +11,37 @@ import {
   ImageBackground
 } from "react-native";
 
+//import isPlayerOnesTurn from "./GameLogic";
+//^this isn't working, I'm not sure how to treat isPlayerOnesTurn as a variable that can be accessed from elsewhere
+
 export default class GameplayScreen extends Component {
+  _onPressButton() {
+    Alert.alert("You tapped the button!");
+  }
+
   render() {
+    //i think the plan is going to be one whole-screen flexbox, with row0, row1, and row2 flexboxes columned inside it
     return (
-      <View style={styles.container}>
+      <View style={styles.container2}>
         <ImageBackground
           source={require("./gridLines.png")}
           style={{ width: "100%", height: "80%" }}
         >
-          <Text>hey</Text>
+          <View style={styles.row}>
+            <Button onPress={this._onPressButton} title="1" />
+            <Button onPress={this._onPressButton} title="2" />
+            <Button onPress={this._onPressButton} title="3" />
+          </View>
+          <View style={styles.row}>
+            <Button onPress={this._onPressButton} title="4" />
+            <Button onPress={this._onPressButton} title="5" />
+            <Button onPress={this._onPressButton} title="6" />
+          </View>
+          <View style={styles.row}>
+            <Button onPress={this._onPressButton} title="7" />
+            <Button onPress={this._onPressButton} title="8" />
+            <Button onPress={this._onPressButton} title="9" />
+          </View>
         </ImageBackground>
       </View>
     );
@@ -33,6 +55,13 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center"
   },
+  container2: {
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "space-evenly"
+  },
   buttonContainer: {
     margin: 20
   },
@@ -44,5 +73,14 @@ const styles = StyleSheet.create({
   },
   normal: {
     fontSize: 14
+  },
+  row: {
+    //flex: 2,
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    //  margin:15
+    height: 30
+    //  width: 30,
+    //  margin:10
   }
 });
